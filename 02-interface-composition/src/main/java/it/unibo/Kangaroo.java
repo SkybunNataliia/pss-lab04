51e1a26bdcbe68c05b0551665fe013ec8ec6cb02
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-10-29 11:13:06 +0100

    Add class Kangaroo that implements Animal

---
+++ 02-interface-composition/src/main/java/it/unibo/Kangaroo.java
@@ -0,0 +1,33 @@
+package it.unibo;
+
+public class Kangaroo implements Animal {
+
+    private static final double WEIGHT = 29.88;
+    private static final boolean VEGETABLES = true;
+
+    private final double averageWeight;
+    private final boolean eatVegetables;
+
+
+    public Kangaroo() {
+        this.averageWeight = WEIGHT;
+        this.eatVegetables = VEGETABLES;
+    }
+
+    public double averageWeight() {
+        return averageWeight;
+    }
+
+    public boolean canEatVegetables() {
+        return eatVegetables;
+    }
+
+    public boolean canEat(Animal a) {
+        if (a.averageWeight() < this.averageWeight) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
