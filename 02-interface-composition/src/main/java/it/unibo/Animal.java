4cd52d59f011287ff584ff46396b9e1c360c10d7
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-10-29 10:59:37 +0100

    Add interface Animal

---
+++ 02-interface-composition/src/main/java/it/unibo/Animal.java
@@ -0,0 +1,13 @@
+package it.unibo;
+
+public interface Animal {
+    // return average weight of an animal in Kg
+    double averageWeight();
+
+    // Return true if animal eats vegetables
+    boolean canEatVegetables();
+
+    // return true if animal can eat the animal passed as parametr
+    // can only eats animals that weight less
+    boolean canEat(Animal a);
+}
