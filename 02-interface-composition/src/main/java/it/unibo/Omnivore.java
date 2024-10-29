9e2cd2346dbfa66f490a3ee1beb7bb17c2098322
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-10-29 11:28:56 +0100

    Modified class Omnivore

--- 02-interface-composition/src/main/java/it/unibo/Omnivore.java
+++ 02-interface-composition/src/main/java/it/unibo/Omnivore.java
@@ -1,7 +1,27 @@
 package it.unibo;
 
 public class Omnivore implements Diet {
+
+    private static final boolean VEGETABLES = true;
+    private static final boolean MEAT = true;
+
+    private final boolean eatVegetables;
+    private final boolean eatMeat;
+
+    public Omnivore() {
+        this.eatVegetables = VEGETABLES;
+        this.eatMeat = MEAT;
+    }
+
     public String getDescription() {
-        return "Bases on both, plants and animals.";
+        return "Bases on both, plants and meat.";
+    }
+
+    public boolean getEatVegetables() {
+        return eatVegetables;
+    }
+
+    public boolean getEatMeat() {
+        return eatMeat;
     }
 }
