b4ea2c1aec063497985af15ef7bb51a23f40579e
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-10-29 11:27:47 +0100

    Modified class Carnivore

--- 02-interface-composition/src/main/java/it/unibo/Carnivore.java
+++ 02-interface-composition/src/main/java/it/unibo/Carnivore.java
@@ -1,7 +1,27 @@
 package it.unibo;
 
 public class Carnivore implements Diet {
+
+    private static final boolean VEGETABLES = false;
+    private static final boolean MEAT = true;
+
+    private final boolean eatVegetables;
+    private final boolean eatMeat;
+
+    public Carnivore() {
+        this.eatVegetables = VEGETABLES;
+        this.eatMeat = MEAT;
+    }
+
     public String getDescription() {
-        return "Bases on animals.";
+        return "Bases on meat.";
+    }
+
+    public boolean getEatVegetables() {
+        return eatVegetables;
+    }
+
+    public boolean getEatMeat() {
+        return eatMeat;
     }
 }
