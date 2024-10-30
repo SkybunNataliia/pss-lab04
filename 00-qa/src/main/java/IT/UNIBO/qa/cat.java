cb181de4a074025ff69280af6f28b238e3519e8e
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-10-30 14:40:29 +0100

    Modified style of class Cat

--- 00-qa/src/main/java/IT/UNIBO/qa/cat.java
+++ 00-qa/src/main/java/IT/UNIBO/qa/cat.java
@@ -1,77 +1,74 @@
 package IT.UNIBO.qa;
 
-public class cat {
-//this class describes a cat
+/**
+ * The class Cat describes a cat.
+ * That can have name, color, breed and age.
+ */
+public class Cat {
 
-    
+    private static int  NINE = 9;
+    static public  int numberoflives= NINE;
 
+    private final String Name;
+    private String Color;
+    private final String Breed;
+    private int Age;
 
-
-public String Name, Color, Breed;
-public int Age;
-
-
-private static int  NINE = 9;
-static public  int numberoflives= NINE;
-
-public cat(String meow,
-final String c,final int age,
-final String korat) {
-    this.Name =meow ;
-    Color=c  ;
-    this.Age=age;
-    Breed= korat;
-}
-
-// This method is useful because it allows the cat to meow and introduce itself to the world in a single line of code.
-    public String
-    Meow()
-    {return "Meow, meow, I am " + Name + ", hear me purr, In this feline world, I'm the star, that's for sure. With whiskers long and fur so sleek, I'm here to share my story, so take a peek!";
-}public
-
-
-
-void setcolor(String c) {
-    this.Color = 
-    c;
-}
-
-    public String GET_COLOR() {
-return this.Color;
+    /**
+     * Constructor of class Cat
+     */
+    public Cat(final String name, String color, final String breed, int age) {
+        this.Name = name;
+        this.Color = color;
+        this.Breed= breed;
+        this.Age = age;
     }
 
-    public int hasSameColor( cat  cat )
-    {
-if (Color==cat.Color) {return   1;}else{return 0;}
+    /**
+     * This method is useful because it allows the cat to meow and
+     * introduce itself to the world in a single line of code.
+     */
+    public String meow() {
+        return "Meow, meow, I am " + this.Name + ", hear me purr, In this feline world, I'm the star, that's for sure. With whiskers long and fur so sleek, I'm here to share my story, so take a peek!";
     }
 
-String name( )
-{
-    return Name;
+    /**
+     * This method allows to set color of the cat.
+     */
+    public void setColor(String color) {
+        this.Color = color;
+    }
+
+    /**
+     * This method return the color of a cat.
+     */
+    public String getColor() {
+        return this.Color;
+    }
+
+    /**
+     * This method return true if the color of the cat is equal to color of the cat passed as parameter.
+     */
+    public boolean hasSameColor(Cat  cat) {
+        return Color==cat.Color;
+    }
+
+    /**
+     * This method return the name of a cat.
+     */
+    String getName() {
+        return this.Name;
+    }
+
+    /**
+     * This method return the breed of a cat.
+     */
+    String breed() {
+        return this.Breed;
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat("Kiri", "Gray", "Koratcat", 4);
+        System.out.println(cat.meow());
+    }
 }
-
-
-String BREED( )
-
-{
-    return 
-    this.Breed;
-}
-
-public
-static
-void
-main(
-    String[] ARGS
-) {
-    cat cat = new cat(
-        "Kiri",
-        "Gray",
-        4, "Kor"+
-         "at");
-
-    System.
-    out.
-    println(cat.Meow());
-}
-        }
